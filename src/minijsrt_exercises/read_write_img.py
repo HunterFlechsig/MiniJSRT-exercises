10e73e49e6e5536edd9c3a0f078c45b1c55ac5fc
Hunter Flechsig <hunterflechsig@gmail.com>
2026-09-10 13:33:09 -0700

    modify existing to code to use uv instead of venv

---
+++ src/minijsrt_exercises/read_write_img.py
@@ -0,0 +1,48 @@
+import cv2
+from utils.utils import (
+    get_files_of_type,
+    load_images_of_type,
+    get_data_dir,
+    write_dicom_to_jpg,
+    write_dicom_to_png
+)
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+current_director = Path.cwd()
+dataDir = get_data_dir()
+png_jpg_dir = dataDir / "Practice_PNGandJPG"
+dicom_dir = dataDir / "Practice_DICOM"
+output_dir = current_director / "read_write"
+
+print(dataDir)
+
+pngFiles = get_files_of_type(png_jpg_dir, 'png')
+jpgFiles = get_files_of_type(png_jpg_dir, 'jpg')
+dcmFiles = get_files_of_type(dataDir, 'dcm')
+
+loadPng = load_images_of_type(pngFiles, 'png')
+loadJpg = load_images_of_type(jpgFiles, 'jpg')
+loadDcm = load_images_of_type(dcmFiles, 'dcm')
+
+
+# Show images
+horizontal_combined = np.hstack((loadPng[0], loadJpg[0]))
+resized = cv2.resize(horizontal_combined, (1000, 500))
+cv2.imshow("show", resized)
+
+plt.imshow(loadDcm[0], cmap="gray")
+plt.show()
+
+# writing Images
+cv2.imwrite(output_dir / "write.png",loadPng[0])
+cv2.imwrite(output_dir / "write.jpg",loadJpg[0])
+
+#write dicom to png and jpg
+write_dicom_to_png(loadDcm[0], output_dir, "writeDicom")
+write_dicom_to_jpg(loadDcm[0], output_dir, "writeDicom")
+
+# close windows when done
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ src/minijsrt_exercises/train_direction_model.py
@@ -0,0 +1,77 @@
+import torch
+import torchvision.models as models
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from utils.utils import ( get_data_dir, get_current_dir )
+from utils.training_utils import ( train_model, evaluate_model )
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
+
+data_dir = get_data_dir() / "Directions01_RGB"
+train_dir = data_dir / "train"
+test_dir = data_dir / "test"
+current_dir = get_current_dir()
+output_dir = current_dir / "read_write"
+
+weights = models.ResNet18_Weights.DEFAULT
+num_classes = 4
+
+# load data
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+train_dataset = ImageFolder(
+    root=train_dir,
+    transform=transform
+)
+
+test_dataset = ImageFolder(
+    root=test_dir,
+    transform=transform
+)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=32,
+    shuffle=True
+)
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=32,
+    shuffle=False
+)
+
+# create ResNet18
+model = models.resnet18(weights = weights)
+model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+criterion = torch.nn.CrossEntropyLoss()
+num_epochs = 5
+
+train_model(
+    model=model,
+    train_loader=train_loader,
+    num_epochs=num_epochs,
+    criterion=criterion,
+    optimizer=optimizer,
+    device=device
+)
+
+# save weights for later analysis
+torch.save(output_dir, 'direction_model_weights.pth')
+
+evaluate_model(
+    model=model,
+    device=device,
+    test_loader=test_loader
+)
+
+
